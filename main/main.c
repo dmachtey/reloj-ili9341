21d1eb217b92ac1e8f0d3d049bda56bd189ad8ce
Esteban Volentini <evolentini@herrera.unt.edu.ar>
2025-03-25 18:00:22 -0300

    Versión inicial del proyecto

--- main/main.c
+++ main/main.c
@@ -27,99 +27,70 @@ SPDX-License-Identifier: MIT
  **
  ** @brief Ejemplo de un led parpadeando
  **
- ** Ejemplo de un led parpadeando utilizando la capa de abstraccion de 
+ ** Ejemplo de un led parpadeando utilizando la capa de abstraccion de
  ** hardware y sin sistemas operativos.
- ** 
+ **
  ** | RV | YYYY.MM.DD | Autor       | Descripción de los cambios              |
  ** |----|------------|-------------|-----------------------------------------|
  ** |  3 | 2025.03.12 | evolentini  | Adaptación para plataforma ESP-32       |
  ** |  2 | 2017.10.16 | evolentini  | Correción en el formato del archivo     |
  ** |  1 | 2017.09.21 | evolentini  | Version inicial del archivo             |
- ** 
+ **
  ** @defgroup ejemplos Proyectos de ejemplo
  ** @brief Proyectos de ejemplo de la Especialización en Sistemas Embebidos
- ** @{ 
+ ** @{
  */
 
-
-/* === Headers files inclusions =============================================================== */
+/* === Headers files inclusions ==================================================================================== */
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
-#include "driver/gpio.h"
+#include "ili9341.h"
+#include "digitos.h"
 
-/* === Macros definitions ====================================================================== */
+/* === Macros definitions =========================================================================================== */
 
-// #define LED_ROJO  GPIO_NUM_10
-// #define LED_VERDE GPIO_NUM_3
-#define LED_AZUL  GPIO_NUM_2
-#define LED_VERDE GPIO_NUM_0
-#define LED_ROJO  GPIO_NUM_1
+#define DIGITO_ANCHO     60
 
-/* === Private data type declarations ========================================================== */
+#define DIGITO_ALTO      100
 
-/** @brief Estructura con los parametros para una tarea de baliza */
-typedef struct {
-    uint8_t led;    /** < Led que debe hacer parpadear la tarea */
-    uint16_t delay; /** < Demora entre cada encendido y apagado */
-} blinking_t;
+#define DIGITO_ENCENDIDO ILI9341_RED
 
-/* === Private variable declarations =========================================================== */
+#define DIGITO_APAGADO   0x3800
 
-/* === Private function declarations =========================================================== */
+#define DIGITO_FONDO     ILI9341_BLACK
 
-/** @brief Función que implementa una tarea de baliza
- **
- ** @parameter[in] parametros Puntero a una estructura que contiene el led
- **                           y la demora entre encendido y apagado.
- */
-void Blinking(void * parametros);
+/* === Private data type declarations =============================================================================== */
 
-/* === Public variable definitions ============================================================= */
+/* === Private variable declarations ================================================================================ */
 
-/* === Private variable definitions ============================================================ */
+/* === Private function declarations ================================================================================ */
 
-static const blinking_t valores[] = {
-    {.led = LED_ROJO, .delay = 500},
-    {.led = LED_VERDE, .delay = 300},
-    {.led = LED_AZUL, .delay = 700},
-};
+/* === Public variable definitions ================================================================================== */
 
-/* === Private function implementation ========================================================= */
+/* === Private variable definitions ================================================================================= */
 
-void Blinking(void * parametros) {
-    const blinking_t * argumentos = parametros;
+/* === Private function implementation ============================================================================== */
 
-    gpio_set_direction(argumentos->led, GPIO_MODE_OUTPUT);
-    gpio_set_level(argumentos->led, 0);
-
-    while (1) {
-        // gpio_set_level(argumentos->led, (gpio_get_level(argumentos->led) != 0));
-        // vTaskDelay(pdMS_TO_TICKS(argumentos->delay));
-
-        gpio_set_level(argumentos->led, 1);
-        vTaskDelay(pdMS_TO_TICKS(argumentos->delay));
-    
-        gpio_set_level(argumentos->led, 0);
-        vTaskDelay(pdMS_TO_TICKS(argumentos->delay));
-    }
-}
-
-/* === Public function implementation ========================================================== */
+/* === Public function implementation =============================================================================== */
 
 void app_main(void) {
-    xTaskCreate(Blinking, "Rojo", configMINIMAL_STACK_SIZE, (void *)&valores[0],
-                tskIDLE_PRIORITY + 1, NULL);
+    ILI9341Init();
+    ILI9341Rotate(ILI9341_Landscape_1);
 
-    xTaskCreate(Blinking, "Verde", configMINIMAL_STACK_SIZE, (void *)&valores[1],
-                tskIDLE_PRIORITY + 1, NULL);
+    panel_t horas = CrearPanel(30, 60, 2, DIGITO_ALTO, DIGITO_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
+    panel_t minutos = CrearPanel(170, 60, 2, DIGITO_ALTO, DIGITO_ANCHO, DIGITO_ENCENDIDO, DIGITO_APAGADO, DIGITO_FONDO);
 
-    xTaskCreate(Blinking, "Azul", configMINIMAL_STACK_SIZE, (void *)&valores[2],
-                tskIDLE_PRIORITY + 1, NULL);
+    DibujarDigito(horas, 0, 1);
+    DibujarDigito(horas, 1, 2);
 
-    while (1);
+    ILI9341DrawFilledCircle(160, 90, 5, DIGITO_ENCENDIDO);
+    ILI9341DrawFilledCircle(160, 130, 5, DIGITO_ENCENDIDO);
+
+    DibujarDigito(minutos, 0, 3);
+    DibujarDigito(minutos, 1, 4);
 }
 
-/* === End of documentation ==================================================================== */
+/* === End of documentation ========================================================================================= */
 
 /** @} End of module definition for doxygen */
